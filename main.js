2404155d4f0a3885f4563c233667405f5b5789b8
Mike Quinn <Ask>
2018-06-21 17:30:20 -0600

    began the menu

--- main.js
+++ main.js
@@ -2,18 +2,69 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 // listen  for the app to be ready
-app.on('ready', () => {
+app.on("ready", () => {
   // create new window
   mainWindow = new BrowserWindow({});
   // load the html file into the window
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'mainWindow.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "mainWindow.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
 });
+
+// this happens in the menu below
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 200,
+    height: 300,
+    title: "Add Shopping List Item"
+  });
+  // load the html file into the window
+  addWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "addWindow.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+}
+
+// Create menu template
+const mainMenuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Add Item",
+        click() {
+          createAddWindow();
+        }
+      },
+      {
+        label: "Clear Items"
+      },
+      {
+        label: "Quit",
+        // aka hotkey, also check for the OS
+        accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
