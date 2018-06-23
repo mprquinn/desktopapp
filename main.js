531e0d39dec333d2160463375c579583002935b3
Mike Quinn <Ask>
2018-06-23 12:25:18 -0600

    can now remove items as well

--- main.js
+++ main.js
@@ -70,7 +70,10 @@ const mainMenuTemplate = [
         }
       },
       {
-        label: "Clear Items"
+        label: "Clear Items",
+        click() {
+          mainWindow.webContents.send("item:clear");
+        }
       },
       {
         label: "Quit",
